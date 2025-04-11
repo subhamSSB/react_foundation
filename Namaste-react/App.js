b3db010afeac76731e477726c5aa564aa3636f65
subham gope <savioursubham@gmail.com>
2025-04-11 23:19:10 +0530

    ignited the app

--- Namaste-react/App.js
+++ Namaste-react/App.js
@@ -1,4 +1,4 @@
 const heading = React.createElement("h1",{},"Hello World From React");
 
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(heading)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
