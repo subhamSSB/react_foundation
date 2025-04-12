073fd37e637e1e789a9a96d4c63e2ac8773e3e4f
subham gope <savioursubham@gmail.com>
2025-04-12 23:46:04 +0530

    created the components

--- Namaste-react/App.js
+++ Namaste-react/App.js
@@ -1,4 +1,16 @@
-const heading = React.createElement("h1",{},"Hello World From React");
+import React from "react"
+import ReactDOM from "react-dom/client";
+
+const Title = () => {
+    return <h1>Namaste React </h1>
+}
+
+const HeadingComponent = () => (
+    <div>
+        <Title/>
+        <h1>Namaste React Functional Components</h1>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading)
+root.render(<HeadingComponent/>)
